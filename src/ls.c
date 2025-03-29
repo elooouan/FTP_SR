86c934c3384b03346695bb3794d41dd213ac703c
Hatoum Elie <hatoume@im2ag-mandelbrot2.u-ga.fr>
2025-03-29 22:18:25 +0100

    Error handling

--- src/ls.c
+++ src/ls.c
@@ -5,7 +5,10 @@ void ls_response(int clientfd)
 	int message_size;
 
 	/* Read message size */
-	Rio_readn(clientfd, &message_size, sizeof(message_size));
+	if(Rio_readn(clientfd, &message_size, sizeof(message_size)) == 0){
+		printf("Lost connection to the server, please try again later.\n");
+        exit(0);
+	};
 	message_size = ntohl(message_size);
 
 	/* Read list of files (ls) from the slave server */
@@ -16,7 +19,10 @@ void ls_response(int clientfd)
 		return;
 	}
 
-	Rio_readn(clientfd, display, message_size);
+	if(Rio_readn(clientfd, display, message_size) == 0){
+		printf("Lost connection to the server, please try again later.\n");
+        exit(0);
+	}
 
 	display[message_size] = '\0';
 
